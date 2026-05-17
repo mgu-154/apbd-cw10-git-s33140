251c5c7ef242c43cc354b37c597c0c522f5bf15a
Maciek Guevara <maciej@guevara.pl>
2026-05-17 21:24:00 +0200

    Dodanie DTO

--- cw10/DTOs/GetPcDto.cs
+++ cw10/DTOs/GetPcDto.cs
@@ -2,5 +2,10 @@
 
 public class GetPcDto
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public float Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
 }
