c9ba718a00fe7622fc7fb4c844650866a39a36c1
Maciek Guevara <maciej@guevara.pl>
2026-05-18 00:57:46 +0200

    Dodanie swaggera oraz implementacja serwisu bazy danych

--- cw10/Program.cs
+++ cw10/Program.cs
@@ -1,9 +1,11 @@
 using cw10.Data;
+using cw10.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<IDbService, DbService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -14,6 +16,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "cw10");
+    });
 }
 
 app.UseAuthorization();
