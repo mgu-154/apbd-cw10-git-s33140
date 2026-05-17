c13f36abcf29fa9ef8e0c208736d46477306c734
Maciek Guevara <maciej@guevara.pl>
2026-05-18 00:56:44 +0200

    Dodanie dto odpowiadajacych za tworzenie i edytowanie PC

---
+++ cw10/DTOs/UpdatePcDto.cs
@@ -0,0 +1,11 @@
+﻿namespace cw10.DTOs;
+
+public class UpdatePcDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public float Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
