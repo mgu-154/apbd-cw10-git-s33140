683d7b7d18bbe57ff70f2d092397eefabe1ef657
Maciek Guevara <maciej@guevara.pl>
2026-05-17 19:30:44 +0200

    Obługa działania programu

---
+++ cw10/Program.cs
@@ -0,0 +1,23 @@
+using cw10.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
