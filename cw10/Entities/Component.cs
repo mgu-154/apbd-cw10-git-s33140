28ec39be239245adca50a2ecb6cb5e79cbc18ea8
Maciek Guevara <maciej@guevara.pl>
2026-05-17 20:31:49 +0200

    Usunięcie wartosci Id

--- cw10/Entities/Component.cs
+++ cw10/Entities/Component.cs
@@ -5,7 +5,6 @@ namespace cw10.Entities;
 
 public class Component
 {
-    public int Id { get; set; }
     public char Code { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
